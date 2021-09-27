f08985be7baddc08e1b3d5479e1e2bbbafd22ed5
yasersharifi <yassersharifi74@gmail.com>
2021-09-27 15:51:58 +0330

    set database connection

--- core/Database.php
+++ core/Database.php
@@ -1,1 +1,19 @@
 <?php
+class Database {
+    private $host = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "phonebook";
+
+    protected $connection;
+
+    public function __construct()
+    {
+        if (! $this->connection) {
+            $this->connection = new mysqli($this->host, $this->username, $this->$this->password, $this->database);
+            return $this->connection;
+        }
+
+        return false;
+    }
+}
